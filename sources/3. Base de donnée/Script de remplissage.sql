511a9b89e55ed0560cf2a50f9adb3e1e533a0322
rubisetcie <rubisetcie@gmail.com>
2019-01-18 14:54:09 +0100

    Ajout du script de remplissage

---
+++ sources/3. Base de donnée/Script de remplissage.sql
@@ -0,0 +1,28 @@
+#------------------------------------------------------------
+#        Script de remplissage MySQL
+#------------------------------------------------------------
+
+# Création des utilisateurs :
+INSERT INTO users
+
+# Créations des manifestations publiées par les membres du BDE :
+INSERT INTO manifestations
+
+# Créations des photos publiées par les étudiants :
+INSERT INTO pictures
+
+# Créations des commentaires sous les photos :
+INSERT INTO comments
+
+# Créations de la liste de toutes les activités :
+INSERT INTO activites
+
+# Création de la boîte à idées :
+INSERT INTO ideas
+
+# Création de la table reliant les activités pratiquées et les utilisateurs :
+INSERT INTO practise
+
+# Création des produits de la boutique :
+INSERT INTO products
+
