33edca7825b348ec137a4402dbaf661a56c1037b
rubisetcie <rubisetcie@gmail.com>
2019-01-21 15:53:08 +0100

    Ajout de la connexion à la base de donnée.

--- api/server.js
+++ api/server.js
@@ -3,15 +3,27 @@
  * Date     : 21/01/2019
  */
 
-// On appelle la bibliothèque "HTTP" :
-var http = require('http');
+// On appelle les bibliothèques :
+var http = require("http");     // Bibliothèque HTTP
+var mysql = require("mysql");   // Bibliothèque MySQL
 
-// On créé le serveur qui répond aux requêtes HTTP :
+// Création du serveur Web :
 var server = http.createServer(function(input, output)
 {
     // On écrit la réponse :
     output.writeHead(200, {"Content-Type" : "text/html"});
     output.end("<p>Salut tout le monde !</p>");
+});
+
+// Connexion au serveur MySQL :
+var database = mysql.createConnection({host : "localhost", user : "root", password : ""});
+
+// On se connecte au serveur :
+database.connect(function(err)
+{
+    // Vérification des erreurs :
+    if (err)
+        throw err;
 });
 
 // On fait écouter le serveur sur le port 8080 :
