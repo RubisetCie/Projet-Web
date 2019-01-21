d9f45f8583fb52675fe40f5b6b758bc987ef6c53
rubisetcie <rubisetcie@gmail.com>
2019-01-21 14:48:06 +0100

    Mise en place du serveur Node.js

---
+++ api/server.js
@@ -0,0 +1,18 @@
+/* Rôle     : Créé le serveur Node.js
+ * Auteur   : Matthieu CARTERON
+ * Date     : 21/01/2019
+ */
+
+// On appelle la bibliothèque "HTTP" :
+var http = require('http');
+
+// On créé le serveur qui répond aux requêtes HTTP :
+var server = http.createServer(function(input, output)
+{
+    // On écrit la réponse :
+    output.writeHead(200, {"Content-Type" : "text/html"});
+    output.end("<p>Salut tout le monde !</p>", );
+});
+
+// On fait écouter le serveur sur le port 8080 :
+server.listen(8080);
