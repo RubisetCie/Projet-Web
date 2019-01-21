64b22c8ba7e5981d21034860f3b922c528938bff
rubisetcie <rubisetcie@gmail.com>
2019-01-21 16:54:04 +0100

    Séparation de la navbar

---
+++ template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!-- Informations d'en-tête -->
+<head>
+    <title>Titre de la page</title>
+    
+    <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="./vendors/fontawesome-free-5.6.3-web/css/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/style.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+    
+    <script src="./vendors/jquery-3.3.1.min.js"></script>
+    <script src="./vendors/bootstrap-4.2.1/js/bootstrap.min.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <header>
+        <!-- Barre de navigation -->
+        <?php
+            require("./res/header.html");
+        ?>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <div>
+            <img class="flotte" src="./res/img/logo_cesi.png" alt="Logo Cesi" height="62.8" width="169">
+        </div>
+    </footer>
+</body>
