690719f5ed8d0a8f812303054d7570a333184f38
Poupiloupe <33811172+Poupiloupe@users.noreply.github.com>
2019-01-22 10:24:50 +0100

    Add files via upload

--- template.php
+++ template.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <!-- Informations d'en-tête -->
 <head>
-    <title>Titre de la page</title>
-    
-    <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="./vendors/fontawesome-free-4.7.0/css/fontawesome.min.css">
-    <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
-    
-    <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
-    
-    <script src="./vendors/jquery-3.3.1.min.js"></script>
-    <script src="./vendors/bootstrap-4.2.1/js/bootstrap.min.js"></script>
 
-    <!-- Balises META -->
-    <meta charset="utf-8">
-    <meta name="description" content="Descritpion de la page">
-    <meta name="keywords" content="CESI,BDE">
-    <meta name="author" content="Killian DEROCHE">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css"> 
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body>
