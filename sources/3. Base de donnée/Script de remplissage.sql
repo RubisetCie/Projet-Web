be99b5c551f5548face6d55f5d05b2d3264b8fa0
rubisetcie <rubisetcie@gmail.com>
2019-01-22 16:30:26 +0100

    Complétion du script de création.

--- sources/3. Base de donnée/Script de remplissage.sql
+++ sources/3. Base de donnée/Script de remplissage.sql
@@ -16,6 +16,16 @@ INSERT INTO comments
 
 # Créations de la liste de toutes les activités :
 INSERT INTO activites
+VALUES
+	("Voile", "Rejoignez-nous à la plage pour faire de la voile entre futur ingénieurs !", "club_voile.jpg"),
+	("Gaming", "Participez à des compétitions de jeux-vidéos en ligne avec les étudiants !", "club_gaming.jpg"),
+	("Cinema", "Développez votre culture du septième art et votre esprit critique en compagnie de futurs ingénieurs !", "club_cinema.jpg"),
+	("Musique", "Organisez des concerts rythmés et devenez un virtuose parmis vos pairs futurs ingénieurs !", ""),
+	("Œnologie", "Retrouvez-nous dans des sorties entre futurs ingénieurs afin de complaire votre culture cullinaire !", "club_oenologie.jpg"),
+	("Basketball", "Intégrez une de nos équipe de basketball et développez votre esprit d'équipe !", "club_basketball.jpg"),
+	("Football", "Rejoignez l'une de nos équipe de football et participez à des compétitions !", ""),
+	("Workout", "Développez votre physique à la salle de sport entre étudiants ingénieurs !", ""),
+	("Soirée", "Participez à des soirées en compagnie du BDE !", "");
 
 # Création de la boîte à idées :
 INSERT INTO ideas
