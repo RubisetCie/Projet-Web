75002e8dc4c6989f4aa354606a2b8dc7caba9161
Poupiloupe <33811172+Poupiloupe@users.noreply.github.com>
2019-01-22 14:09:31 +0100

    Add files via upload

---
+++ associations.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!-- Informations d'en-tête -->
+<head>
+
+ <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css"> 
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
+<link rel="stylesheet" type="text/css" href="./style/css/association.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+    <header>
+        <!-- Barre de navigation -->
+        <?php
+            require("./res/header.html");
+        ?>
+    </header>
+
+    <main>
+<?php
+            require("./res/association.html");
+        ?>
+    </main>
+
+    <footer>
+        <div>
+            <img class="flotte" src="./res/img/logo_cesi.png" alt="Logo Cesi" height="62.8" width="169">
+        </div>
+    </footer>
+</body>
