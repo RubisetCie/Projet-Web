1b40a925028c7422927ed502ebee043abd802038
rubisetcie <rubisetcie@gmail.com>
2019-01-22 16:44:54 +0100

    Correction du carousel pour la boutique

--- shop.php
+++ shop.php
@@ -40,15 +40,15 @@
             <section class="col-12 col-lg-8">
                 <div class="shop-section widget widget-primary shop-jumbotron">
                     <h2 class="shop-jumbotron-title">Bienvenue</h2>
-                    <h3 class="widget-offset shop-jumbotron-lead">Dans la boutique du BDE</h3>
+                    <h3 class="widget-offset shop-jumbotron-lead">Dans la boutique de goodies du BDE !</h3>
                     <form class="row shop-search-form">
-                        <input type="text" placeholder="Search" class="form-control col-11" />
+                        <input type="text" placeholder="Rechercher un article" class="form-control col-11" />
                         <button class="button button-invisible col-1">
                             <i class="fa fa-search"></i>
                         </button>
                     </form>
                     
-                    <div id="topvente" class="carousel slide" data-ride="carousel">
+                    <div id="topvente" class="shop-carousel carousel slide" data-ride="carousel">
                         <!-- Indicateurs -->
                         <ul class="carousel-indicators">
                             <li data-target="#topvente" data-slide-to="0" class="active"></li>
@@ -58,23 +58,37 @@
 
                         <!-- Les images -->
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="./res/img/2.jpg" alt="Top Vente 1">
+                            <div class="item active">
+                                <img src="./res/img/2.jpg" align="center" alt="Top Vente 1">
+                                <div class="carousel-caption">
+                                    <h3>Article 1</h3>
+                                    <p>Description</p>
+                                </div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="./res/img/4.jpg" alt="Top Vente 2">
+                            <div class="item">
+                                <img src="./res/img/4.jpg" align="center" alt="Top Vente 2">
+                                <div class="carousel-caption">
+                                    <h3>Article 1</h3>
+                                    <p>Description</p>
+                                </div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="./res/img/6.jpg" alt="Top Vente 3">
+                            <div class="item">
+                                <img src="./res/img/6.jpg" align="center" alt="Top Vente 3">
+                                <div class="carousel-caption">
+                                    <h3>Article 1</h3>
+                                    <p>Description</p>
+                                </div>
                             </div>
                         </div>
 
                         <!-- Les contrôles gauche et droite -->
-                        <a class="carousel-control-prev" href="#topvente" data-slide="prev">
-                            <span class="carousel-control-prev-icon"></span>
+                        <a class="left carousel-control" href="#topvente" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Précedent</span>
                         </a>
-                        <a class="carousel-control-next" href="#topvente" data-slide="next">
-                            <span class="carousel-control-next-icon"></span>
+                        <a class="right carousel-control" href="#topvente" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Suivant</span>
                         </a>
                     </div>
                 </div>
