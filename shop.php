64538f0e2dbcc17f6a9ba510e48464d683232551
rubisetcie <rubisetcie@gmail.com>
2019-01-22 10:52:17 +0100

    MAJ du template

--- shop.php
+++ shop.php
@@ -1,179 +1,38 @@
 <!DOCTYPE html>
-<html>
-
-<!-- Mirrored from localhost:8081/shop.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 18 Apr 2017 15:54:59 GMT -->
-<!-- Added by HTTrack -->
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
-
 <head>
-    <meta charset="utf-8" />
-    <title>Shop - BDE Cesi</title>
-    <meta name="viewport" content="width=device-width;user-scalable=no;initial-scale=1">
-    <link rel="stylesheet" href="static/css/bootstrap.css" />
-    <link rel="stylesheet" href="static/css/fa/css/font-awesome.css" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
-    <link rel="icon" href="static/img/favicon.png" type="image/png">
-    <link rel="stylesheet" type="text/css" href="BDE/BDE/StaticExport/static/css/base.css" />
-    <link rel="stylesheet" type="text/css" href="BDE/BDE/StaticExport/static/css/foothead.css" />
-    <link rel="stylesheet" type="text/css" href="BDE/BDE/StaticExport/static/css/shop.css" />
-     <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
-
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Boutique</title>
+    
     <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    
+    <!-- On inclus les deux versions de bootstrap ! -->
+    <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/bootstrap-3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/fontawesome-free-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/base.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/foothead.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/shop.css">
+    <link rel="icon" href="static/img/favicon.png" type="image/png">
+
+    <script src="./vendors/jquery-3.3.1.min.js"></script>
+    <script src="./vendors/bootstrap-4.2.1/js/bootstrap.min.js"></script>
+    
+    <!-- Balises META -->
+    <meta charset="utf-8">
+    <meta name="description" content="Descritpion de la page">
+    <meta name="keywords" content="CESI,BDE">
+    <meta name="author" content="Killian DEROCHE">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
     <header>
-
-        <!-- navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-
-            <div>
-                <img src="./img/logo_bde.png" alt="Logo bde" height="69.6" width="58">
-            </div>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item text-nav">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item text-nav">
-                        <a class="nav-link" href="#">Magasin</a>
-                    </li>
-                    <li class="nav-item text-nav">
-                        <a class="nav-link" href="#">Association</a>
-                    </li>
-                    <li class="nav-item text-nav">
-                        <a class="nav-link" href="#">Boite à idée</a>
-                    </li>
-                </ul>
-            </div>
-
-
-
-
-
-
-            <!----------------- Search ------------------------->
-            <form class="navbar-form navbar-right inline-form">
-                <div class="form-group modal-box">
-                    <input type="search" class="input-sm form-control" placeholder="Recherche">
-                    <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
-                </div>
-            </form>
-
-            <!----------------- Se connecter ------------------------->
-            <div class="text-center modal-box">
-                <!-- Button HTML (to Trigger Modal) -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Modal1">
-                    Se connecter
-                </button>
-
-            </div>
-
-            <!-- Modal Formulaire connexion -->
-            <div id="Modal1" class="modal fade">
-                <div class="modal-dialog modal-login">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Connexion BDE</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="/examples/actions/confirmation.php" method="post">
-                                <div class="form-group">
-                                    <i class="fa fa-user"></i>
-                                    <input type="text" class="form-control" placeholder="Username" required="required">
-                                </div>
-                                <div class="form-group">
-                                    <i class="fa fa-lock"></i>
-                                    <input type="password" class="form-control" placeholder="Password" required="required">
-                                </div>
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary btn-block btn-lg" value="Login">
-                                </div>
-                            </form>
-
-                        </div>
-                        <div class="modal-footer">
-                            <a href="#">Mot de passe oublié ?</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!----------------------- INSCRIPTION ----------------------------->
-            <div class="text-center">
-                <!-- Button HTML (to Trigger Modal) -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Modal2">
-                    S'inscrire ?
-                </button>
-            </div>
-
-            <!-- Modal Formulaire inscription -->
-            <div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-
-                            <div class="signup-form">
-                                <form action="/examples/actions/confirmation.php" method="post">
-                                    <h2>Inscription</h2>
-                                    <p class="hint-text">Creer ton compte BDE</p>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-xs-6"><input type="text" class="form-control" name="Nom" placeholder="Nom" required="required"></div>
-                                            <div class="col-xs-6"><input type="text" class="form-control" name="Prénom" placeholder="Prénom" required="required"></div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" name="email" placeholder="Email" required="required">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="Promotion" placeholder="Promotion" required="required">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
-                                    </div>
-                                </form>
-                                <div class="text-center">Already have an account? <a href="#">Sign in</a></div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-        </nav>
-
+        <!-- Barre de navigation -->
+        <?php
+            require("./res/header.html");
+        ?>
     </header>
 
     <section class="container main-page main-page-pad">
@@ -478,6 +337,3 @@
         </div>
     </footer>
 </body>
-
-<!-- Mirrored from localhost:8081/shop.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 18 Apr 2017 15:54:59 GMT -->
-</html>
