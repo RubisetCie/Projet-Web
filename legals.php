1247afd67b780045e16ee89b0074a348d612baac
rubisetcie <rubisetcie@gmail.com>
2019-01-23 11:24:29 +0100

    Ajout des templates des pages vides à créer

---
+++ legals.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!-- Informations d'en-tête -->
+<head>
+    <title>Titre de la page</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+    
+    <!-- On inclus les deux versions de bootstrap ! -->
+    <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/bootstrap-3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/base.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
+
+    <script src="./vendors/jquery-3.3.1.min.js"></script>
+    <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+    
+    <!-- Balises META -->
+    <meta charset="utf-8">
+    <meta name="description" content="Descritpion de la page">
+    <meta name="keywords" content="CESI,BDE">
+    <meta name="author" content="Matthieu CARTERON">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <header>
+        <!-- Barre de navigation -->
+        <?php
+            require("./res/header.html");
+        ?>
+    </header>
+
+    <main>
+    </main>
+
+    <footer class="container-fluid footer">
+        <!-- Pied de page -->
+        <?php
+            require("./res/footer.html");
+        ?>
+    </footer>
+</body>
