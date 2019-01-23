2e72be281c4bf8577c8e3686e55a8b11161d9336
rubisetcie <rubisetcie@gmail.com>
2019-01-23 16:34:27 +0100

    Ajout de la liste des pages

--- legals.php
+++ legals.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <!-- Informations d'en-tête -->
+
 <head>
-    <title>Titre de la page</title>
-    
+    <title>Boite à idées - BDE Cesi</title>
+
     <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
-    
+
     <!-- On inclus les deux versions de bootstrap ! -->
     <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./vendors/bootstrap-3.3.7/css/bootstrap.min.css">
@@ -12,10 +13,12 @@
     <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
     <link rel="stylesheet" type="text/css" href="./style/css/base.css">
     <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/ideabox.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/legalmentions.css">
 
     <script src="./vendors/jquery-3.3.1.min.js"></script>
     <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
-    
+
     <!-- Balises META -->
     <meta charset="utf-8">
     <meta name="description" content="Descritpion de la page">
@@ -33,7 +36,42 @@
     </header>
 
     <main>
+
+        <p>
+            Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.<br>
+
+            Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.<br>
+
+            Cum saepe multa, tum memini domi in hemicyclio sedentem, ut solebat, cum et ego essem una et pauci admodum familiares, in eum sermonem illum incidere qui tum forte multis erat in ore. Meministi enim profecto, Attice, et eo magis, quod P. Sulpicio utebare multum, cum is tribunus plebis capitali odio a Q. Pompeio, qui tum erat consul, dissideret, quocum coniunctissime et amantissime vixerat, quanta esset hominum vel admiratio vel querella.<br>
+
+            Illud tamen clausos vehementer angebat quod captis navigiis, quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi vero solitarum rerum cibos iam consumendo inediae propinquantis aerumnas exitialis horrebant.<br>
+
+            Tu autem, Fanni, quod mihi tantum tribui dicis quantum ego nec adgnosco nec postulo, facis amice; sed, ut mihi videris, non recte iudicas de Catone; aut enim nemo, quod quidem magis credo, aut si quisquam, ille sapiens fuit. Quo modo, ut alia omittam, mortem filii tulit! memineram Paulum, videram Galum, sed hi in pueris, Cato in perfecto et spectato viro.<br>
+
+            Saepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inopiam desiderata sit amicitia, ut dandis recipiendisque meritis quod quisque minus per se ipse posset, id acciperet ab alio vicissimque redderet, an esset hoc quidem proprium amicitiae, sed antiquior et pulchrior et magis a natura ipsa profecta alia causa. Amor enim, ex quo amicitia nominata est, princeps est ad benevolentiam coniungendam. Nam utilitates quidem etiam ab iis percipiuntur saepe qui simulatione amicitiae coluntur et observantur temporis causa, in amicitia autem nihil fictum est, nihil simulatum et, quidquid est, id est verum et voluntarium.<br>
+
+            Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.<br>
+
+            Saraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.<br>
+
+            Haec subinde Constantius audiens et quaedam referente Thalassio doctus, quem eum odisse iam conpererat lege communi, scribens ad Caesarem blandius adiumenta paulatim illi subtraxit, sollicitari se simulans ne, uti est militare otium fere tumultuosum, in eius perniciem conspiraret, solisque scholis iussit esse contentum palatinis et protectorum cum Scutariis et Gentilibus, et mandabat Domitiano, ex comite largitionum, praefecto ut cum in Syriam venerit, Gallum, quem crebro acciverat, ad Italiam properare blande hortaretur et verecunde.<br>
+
+            Emensis itaque difficultatibus multis et nive obrutis callibus plurimis ubi prope Rauracum ventum est ad supercilia fluminis Rheni, resistente multitudine Alamanna pontem suspendere navium conpage Romani vi nimia vetabantur ritu grandinis undique convolantibus telis, et cum id inpossibile videretur, imperator cogitationibus magnis attonitus, quid capesseret ambigebat.<br>
+
+            Nec minus feminae quoque calamitatum participes fuere similium. nam ex hoc quoque sexu peremptae sunt originis altae conplures, adulteriorum flagitiis obnoxiae vel stuprorum. inter quas notiores fuere Claritas et Flaviana, quarum altera cum duceretur ad mortem, indumento, quo vestita erat, abrepto, ne velemen quidem secreto membrorum sufficiens retinere permissa est. ideoque carnifex nefas admisisse convictus inmane, vivus exustus est.<br>
+
+            Et Epigonus quidem amictu tenus philosophus, ut apparuit, prece frustra temptata, sulcatis lateribus mortisque metu admoto turpi confessione cogitatorum socium, quae nulla erant, fuisse firmavit cum nec vidisset quicquam nec audisset penitus expers forensium rerum; Eusebius vero obiecta fidentius negans, suspensus in eodem gradu constantiae stetit latrocinium illud esse, non iudicium clamans.<br>
+
+            Tu autem, Fanni, quod mihi tantum tribui dicis quantum ego nec adgnosco nec postulo, facis amice; sed, ut mihi videris, non recte iudicas de Catone; aut enim nemo, quod quidem magis credo, aut si quisquam, ille sapiens fuit. Quo modo, ut alia omittam, mortem filii tulit! memineram Paulum, videram Galum, sed hi in pueris, Cato in perfecto et spectato viro.<br>
+
+            Nam quibusdam, quos audio sapientes habitos in Graecia, placuisse opinor mirabilia quaedam (sed nihil est quod illi non persequantur argutiis): partim fugiendas esse nimias amicitias, ne necesse sit unum sollicitum esse pro pluribus; satis superque esse sibi suarum cuique rerum, alienis nimis implicari molestum esse; commodissimum esse quam laxissimas habenas habere amicitiae, quas vel adducas, cum velis, vel remittas; caput enim esse ad beate vivendum securitatem, qua frui non possit animus, si tamquam parturiat unus pro pluribus.<br>
+
+            Cuius acerbitati uxor grave accesserat incentivum, germanitate Augusti turgida supra modum, quam Hannibaliano regi fratris filio antehac Constantinus iunxerat pater, Megaera quaedam mortalis, inflammatrix saevientis adsidua, humani cruoris avida nihil mitius quam maritus; qui paulatim eruditiores facti processu temporis ad nocendum per clandestinos versutosque rumigerulos conpertis leviter addere quaedam male suetos falsa et placentia sibi discentes, adfectati regni vel artium nefandarum calumnias insontibus adfligebant.<br>
+        </p>
+
+
     </main>
+
 
     <footer class="container-fluid footer">
         <!-- Pied de page -->
--- shop.php
+++ shop.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" type="text/css" href="./style/css/base.css">
     <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
     <link rel="stylesheet" type="text/css" href="./style/css/shop.css">
-    <link rel="icon" href="static/img/favicon.png" type="image/png">
 
     <script src="./vendors/jquery-3.3.1.min.js"></script>
     <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
@@ -36,7 +35,7 @@
     <section class="container main-page main-page-pad">
         <div class="shop row">
             <section class="col-12 col-lg-8">
-                <div class="a">
+                <div class="shop-section">
                     <h2 class="shop-jumbotron-title">Bienvenue</h2>
                     <h3 class="widget-offset shop-jumbotron-lead">Dans la boutique de goodies du BDE !</h3>
                     <form class="row shop-search-form">
@@ -95,7 +94,7 @@
                     <!-- Article statique -->
                     <div class="product widget col-auto">
                         <div class="product-image">
-                            <img src="./res/img/2.jpg" alt="MyItem 2" />
+                            <img src="./res/img/2.jpg" alt="MyItem 2"/>
                         </div>
                         <div class="product-desc">
                             <h4 class="product-title">
@@ -108,183 +107,11 @@
                             </div>
                         </div>
                     </div>
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/3.jpg" alt="MyItem 3" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 3 </h4>
-                            <div class="product-price">
-                                40€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/4.jpg" alt="MyItem 4" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 4 </h4>
-                            <div class="product-price">
-                                10€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/5.jpg" alt="MyItem 5" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 5 </h4>
-                            <div class="product-price">
-                                55€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/6.jpg" alt="MyItem 6" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 6 </h4>
-                            <div class="product-price">
-                                46€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/7.jpg" alt="MyItem 7" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 7 </h4>
-                            <div class="product-price">
-                                44€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/8.jpg" alt="MyItem 8" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 8 </h4>
-                            <div class="product-price">
-                                74€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/9.jpg" alt="MyItem 9" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 9 </h4>
-                            <div class="product-price">
-                                113€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/10.jpg" alt="MyItem 10" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 10 </h4>
-                            <div class="product-price">
-                                107€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/11.jpg" alt="MyItem 11" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 11 </h4>
-                            <div class="product-price">
-                                76€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
-
-
-                    <article class="product widget col-auto">
-                        <div class="product-image">
-                            <img src="./img/12.jpg" alt="MyItem 12" />
-                        </div>
-                        <div class="product-desc">
-                            <h4 class="product-title">
-                                MyItem 12 </h4>
-                            <div class="product-price">
-                                115€
-                                <nav class="product-menu">
-                                    <a href="#"><i class="fa fa-cart-plus fa-3x"></i></a>
-                                </nav>
-                            </div>
-                        </div>
-                    </article>
                 </div>
             </section>
 
-            <aside >
-                <div class="button">
-                    <br/>
-                    <br/>
-                    <button class="button button-primary" style="width:250px" >Passer la commande</button>
-                </div>
+            <aside>
+                <button class="button button-primary" style="width:250px;height:150px">Passer commande</button>
             </aside>
         </div>
 
