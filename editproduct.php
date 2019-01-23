e6b1ff5c37dabd0016eb19052c5fcb80d2e2d9a0
rubisetcie <rubisetcie@gmail.com>
2019-01-23 16:42:31 +0100

    Formattage de la page d'édition de produit

--- editproduct.php
+++ editproduct.php
@@ -35,59 +35,54 @@
             <h1>Edition du Produit</h1>
             <hr>
             <div class="row">
-            <!-- left column -->
-            <div class="col-md-3">
-              <div class="text-center">
-                <img src="//placehold.it/100" class="avatar img-circle" alt="avatar"><!-- le src de cette photo se trouve directement sur internet-->
-                <h6>télécharger une photo...</h6>
+                <!-- Colonne de gauche -->
+                <div class="col-md-3">
+                    <div class="text-center">
+                        <img src="//placehold.it/100" class="avatar img-circle" alt="avatar"> <!-- le src de cette photo se trouve directement sur internet-->
+                        <h6>Télécharger une photo...</h6>
 
-                <input type="file" class="form-control">
-              </div>
-            </div>
+                        <input type="file" class="form-control">
+                    </div>
+                </div>
       
-      <!-- edit form column -->
-      <div class="col-md-9 personal-info">
-    
-        <h3>Informations du produit</h3>
-        
-        <form class="form-horizontal" role="form">
-          <div class="form-group">
-            <label class="col-lg-3 control-label">Nom:</label>
-            <div class="col-lg-8"><!--col-lg-8 permet de definir la longueur de la bar de l'input, ici la longueur est de 8-->
-              <input class="form-control" type="text">
+                <!-- Colonne d'édition produit -->
+                <div class="col-md-9 personal-info">
+                    <h3>Informations du produit</h3>
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label">Nom :</label>
+                            <div class="col-lg-8"><!--col-lg-8 permet de definir la longueur de la bar de l'input, ici la longueur est de 8-->
+                                <input class="form-control" type="text">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label">Catégorie :</label>
+                            <div class="col-lg-8">
+                                <input class="form-control" type="text">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Prix(€) :</label>
+                            <div class="col-md-2">
+                                <input class="form-control" type="text" >
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label" >Description :</label>
+                            <div class="col-lg-6">
+                                <input class="input_description" type="text" style="width: 600px; height: 120px;">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label"></label>
+                            <div class="col-md-8">
+                                <input type="button" class="btn btn-warning" value="Enregistrer"> 
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </div>
-          <div class="form-group">
-            <label class="col-lg-3 control-label">catégorie:</label>
-            <div class="col-lg-8">
-              <input class="form-control" type="text">
-            </div>
-          </div>
-             <div class="form-group">
-            <label class="col-md-3 control-label">Prix(€):</label>
-            <div class="col-md-2">
-              <input class="form-control" type="text" >
-            </div>
-          </div>
-            <div class="form-group">
-            <label class="col-lg-3 control-label" >description:</label>
-            <div class="col-lg-6">
-              <input class="input_description" type="text" style="width: 600px; height: 120px;">
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="col-md-3 control-label"></label>
-            <div class="col-md-8">
-              <input type="button" class="btn btn-warning" value="enregistrer"> 
-            </div>
-          </div>
-        </form>
-      </div>
-  </div>
-</div>
-         
-
-        
+        </div>
     </main>
 
     <footer class="container-fluid footer">
