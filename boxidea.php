bd7a7da4cdd5101000711d3061268786df9ee128
SalimZemri <33152902+SalimZemri@users.noreply.github.com>
2019-01-23 15:22:11 +0100

    Add files via upload

---
+++ boxidea.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<!-- Informations d'en-tête -->
+
+<head>
+    <title>Titre de la page</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+
+    <!-- On inclus les deux versions de bootstrap ! -->
+    <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/bootstrap-3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./vendors/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/base.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/ideabox.css">
+
+    <script src="./vendors/jquery-3.3.1.min.js"></script>
+    <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- Balises META -->
+    <meta charset="utf-8">
+    <meta name="description" content="Descritpion de la page">
+    <meta name="keywords" content="CESI,BDE">
+    <meta name="author" content="Matthieu CARTERON">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <header>
+        <!-- Barre de navigation -->
+        <?php
+            require("./res/header.html");
+        ?>
+    </header>
+
+    <main>
+
+        <section>
+
+            <p class="idea">
+                Dum haec in oriente aguntur, Arelate hiemem agens Constantius post theatralis ludos atque circenses ambitioso editos apparatu diem sextum idus Octobres, qui imperii eius annum tricensimum terminabat, insolentiae pondera gravius librans, siquid dubium deferebatur aut falsum, pro liquido accipiens et conperto, inter alia excarnificatum Gerontium Magnentianae comitem partis exulari maerore multavit.
+            </p>
+
+
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+            <p class="idea">
+                Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.
+            </p>
+
+            
+
+
+        </section>
+
+        <aside>
+
+            <img src="https://risibank.fr/cache/stickers/d1076/107688-full.gif">
+
+            <p class="ideatop">Dum haec in oriente aguntur.</p>
+
+        </aside>
+
+    </main>
+    
+
+    <footer class="container-fluid footer">
+        <!-- Pied de page -->
+        <?php
+            require("./res/footer.html");
+        ?>
+    </footer>
+</body>
