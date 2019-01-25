5e5e6f5f9e2fa1aa53f9eb4318b4edde34e95a32
SalimZemri <33152902+SalimZemri@users.noreply.github.com>
2019-01-25 14:22:41 +0100

    Essai du code PHP pour les mails auto (à modifier)

---
+++ php/sendmail.php
@@ -0,0 +1,32 @@
+<?php
+
+    require_once('phpmailer/PHPMailerAutoload.php');
+
+    $mail = new PHPMailer();
+    $mail ->isSMTP();
+    //$mail ->SMTPAuth();
+    $mail ->SMTPSecure = 'ssl';
+    $mail ->Host = 'smtp.gmail.com';
+    $mail ->Port = '465';
+    $mail ->isHTML = true;
+    $mail ->Username = 'testphp2135@gmail.com';
+    $mail ->Password = 'azerty1-';
+    $mail ->SetFrom('testphp2135@gmail.com');
+    $mail ->Subject = 'Hi';
+    $mail ->Body = 'body';
+    $mail ->AddAddress('testphp2135@gmail.com');
+    
+    $mail ->Send();
+        
+    if ($mail ->Send())
+    {
+        echo "Yes";
+    }
+
+    if (!$mail ->Send())
+    {
+        echo "No";
+    }
+
+
+?>
