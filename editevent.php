e51c0a191fb9edf53aaf4c363c5594aea5d5b818
Poupiloupe <33811172+Poupiloupe@users.noreply.github.com>
2019-01-25 11:19:03 +0100

    Add files via upload

--- editevent.php
+++ editevent.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
-<!-- Informations d'en-tête -->
 <head>
-    <title>Titre de la page</title>
+    <title>Nouvel événement</title>
     
     <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
     
@@ -12,15 +11,18 @@
     <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
     <link rel="stylesheet" type="text/css" href="./style/css/base.css">
     <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/forms.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/edition.css">
 
     <script src="./vendors/jquery-3.3.1.min.js"></script>
     <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
-    
+    <script src="./js/date.js"></script>
+    <script src="./js/uploadfile.js"></script>
+
     <!-- Balises META -->
     <meta charset="utf-8">
-    <meta name="description" content="Descritpion de la page">
-    <meta name="keywords" content="CESI,BDE">
-    <meta name="author" content="Matthieu CARTERON">
+    <meta name="description" content="Permet d'ajouter un nouvel événement">
+    <meta name="author" content="Killian DEROCHE">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -33,12 +35,66 @@
     </header>
 
     <main>
-    </main>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content bg-color">
+                <div class="modal-header">
+                    <h5 class="or">Editer une Association</h5>
+                </div>
+                
+                <div class="form-group">
+                    <label for="Tire_assoc" class="col-xs-2 control-label">Titre:</label>
+                        <div class="col-xs-9">
+                        <input type="text" class="form-control" id="Titre_assoc" name="Tire_assoc" placeholder="Ecrire le titre">
+                        </div>
+                </div>
 
+                <div class="container">
+                    <div class="row">
+                         <label for="Image" class="col-xs-1 control-label">Image:</label>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="input-group">
+                                <label class="input-group-btn">
+                                    <span class="btn btn-orange">
+                                        Choisir&hellip; <input type="file" id="Image"style="display: none;" multiple >
+                                    </span>
+                                </label>
+                                <input type="text" class="form-control" readonly>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+					
+					           
+      
+                <div class="form-group green-border-focus">
+                  <label for="Description" class="col-xs-1 control-label">Description:</label>
+                  <textarea class="form-control" id="Description" rows="2" placeholder="Ecrivez la description de l'association"></textarea>
+                </div>
+
+
+            <div>
+                	<!-- Material inline 1 -->
+				<div class="form-check form-check-inline">
+				  <input type="radio" class="form-check-input" id="materialInline1" name="inlineMaterialRadiosExample">
+				  <label class="form-check-label" for="materialInline1">Gratuit</label>
+				</div>
+
+				<!-- Material inline 2 -->
+				<div class="form-check form-check-inline">
+				  <input type="radio" class="form-check-input" id="materialInline2" name="inlineMaterialRadiosExample">
+				  <label class="form-check-label" for="materialInline2">Payant</label>
+				</div>
+
+            </div>
+
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success">Soumettre</button>
+                </div>
+            </div>
+        </div>
+    </main>
+    
     <footer class="container-fluid footer">
-        <!-- Pied de page -->
-        <?php
-            require("./res/footer.html");
-        ?>
     </footer>
 </body>
