5cac7dc6f621ba9fe33dc428ee6b4ec1e79fe559
rubisetcie <rubisetcie@gmail.com>
2019-01-25 09:19:38 +0100

    Ajout des conditions générales de vente

--- cgu.php
+++ cgu.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <!-- Informations d'en-tête -->
 <head>
-    <title>Titre de la page</title>
-    
+    <title>Conditions générales de vente</title>
+
     <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
-    
+
     <!-- On inclus les deux versions de bootstrap ! -->
     <link rel="stylesheet" href="./vendors/bootstrap-4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./vendors/bootstrap-3.3.7/css/bootstrap.min.css">
@@ -12,14 +12,15 @@
     <link rel="stylesheet" type="text/css" href="./style/css/navbar.css">
     <link rel="stylesheet" type="text/css" href="./style/css/base.css">
     <link rel="stylesheet" type="text/css" href="./style/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="./style/css/legalmentions.css">
 
     <script src="./vendors/jquery-3.3.1.min.js"></script>
     <script src="./vendors/bootstrap-3.3.7/js/bootstrap.min.js"></script>
-    
+
     <!-- Balises META -->
     <meta charset="utf-8">
-    <meta name="description" content="Descritpion de la page">
-    <meta name="keywords" content="CESI,BDE">
+    <meta name="description" content="Conditions générales de vente pour la boutique">
+    <meta name="keywords" content="CESI,BDE,Conditions,Générales,Ventes">
     <meta name="author" content="Matthieu CARTERON">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -33,6 +34,21 @@
     </header>
 
     <main>
+        <p>
+            Entre la société CESI BDE, 93 Boulevard de la Seine, 92000 Nanterre, au Capital Social de 25 000 €, immatriculée au Registre du Commerce et des Sociétés de PARIS, sous le numéro SIRET 123456789, représentée par M. Mohamed BELGACEM en qualité de gérant, dûment habilité aux fins des présentes.<br>
+            La société peut être jointe par email en cliquant sur le formulaire de contact accessible via la page d’accueil du site.<br>
+            Ci-après le « Vendeur » ou la « Société ».<br>
+            D’une part,<br>
+            Et la personne physique ou morale procédant à l’achat de produits ou services de la société,<br>
+            Ci-après, « l’acheteur », ou « le client »<br>
+            D’autre part,<br>
+            <br>
+            Il a été exposé et convenu ce qui suit :<br>
+            <br>
+            Le Vendeur est éditeur de goodies à destination de collectionneurs, commercialisés par l’intermédiaire de son site Internet (https://www.cesi-bde.fr ). La liste et le descriptif des biens et services proposés par la Société peuvent être consultés sur les sites susmentionnés.<br>
+            <br>
+            Les prix des produits vendus au travers des sites Internet sont indiqués en Euros hors taxes et précisément déterminés sur les pages de descriptifs des Produits. Ils sont également indiqués en euros toutes taxes comprises (TVA + autres taxes éventuelles) sur la page de commande des produits, et hors frais spécifiques d’expédition. Pour tous les produits expédiés hors Union européenne et/ou DOM-TOM, le prix est calculé hors taxes automatiquement sur la facture. Des droits de douane ou autres taxes locales ou droits d’importation ou taxes d’état sont susceptibles d’être exigibles dans certains cas. Ces droits et sommes ne relèvent pas du ressort du Vendeur. Ils seront à la charge de l’acheteur et relèvent de sa responsabilité (déclarations, paiement aux autorités compétentes, etc.). Le Vendeur invite à ce titre l’acheteur à se renseigner sur ces aspects auprès des autorités locales correspondantes.La Société se réserve la possibilité de modifier ses prix à tout moment pour l’avenir. Les frais de télécommunication nécessaires à l’accès aux sites Internet de la Société sont à la charge du Client. Le cas échéant également, les frais de livraison.
+        </p>
     </main>
 
     <footer class="container-fluid footer">
