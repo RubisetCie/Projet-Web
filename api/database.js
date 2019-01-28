a95e96ed9dd7da1701e6070e8cb7e466010975a5
rubisetcie <rubisetcie@gmail.com>
2019-01-28 20:40:58 +0100

    Création l'API Rest.

--- api/database.js
+++ api/database.js
@@ -4,25 +4,49 @@
  * Date     : 21/01/2019
  */
 
-class Database
+module.exports =
 {
-    static HOST = "localhost";
-    static USER = "root";
-    static PASSWORD = "";
-    
-    static connect()
+    // Permet de se connecter à la base de données :
+    connect : function()
     {
         var mysql = require("mysql");
 
         // Connexion au serveur MySQL :
-        var database = mysql.createConnection({host : HOST, user : USER, password : PASSWORD});
+        this.database = mysql.createConnection({host : "localhost", user : "root", password : "", database : "cesiprojet"});
 
         // On se connecte au serveur :
-        database.connect(function(err)
+        this.database.connect(function(err)
         {
             // Vérification des erreurs :
             if (err)
                 throw err;
         });
+    },
+
+    // Récupère une table complète :
+    getAll : function(table, callback)
+    {
+        this.database.query("SELECT * FROM " + table, function(err, result)
+        {
+            // Vérification des erreurs :
+            if (err)
+                throw err;
+
+            callback(JSON.stringify(result));
+        });
+    },
+
+    // Récupère une ligne d'une table :
+    getID : function(table, key, value, callback)
+    {
+        this.database.query("SELECT * FROM " + table + " WHERE " + key + " = " + value, function(err, result)
+        {
+            // Vérification des erreurs :
+            if (err)
+                throw err;
+
+            callback(JSON.stringify(result));
+        });
     }
-}
+};
+
--- api/server.js
+++ api/server.js
@@ -4,20 +4,38 @@
  * Date     : 21/01/2019
  */
 
-import Database from "database.js";
-
-var http = require("http");
-
-// Création du serveur Web :
-var server = http.createServer(function(input, output)
-{
-    // On écrit la réponse :
-    output.writeHead(200, {"Content-Type" : "text/html"});
-    output.end("<p>Salut tout le monde !</p>");
-});
+var express = require("express");
+var fs = require("fs");
+var database = require("./database");
+var app = express();
 
 // On se connecte à la base de donnée :
-Database.connect();
+database.connect();
+
+// On définit les requêtes GET pour récupérer des données depuis la base :
+app.get("/:table", function(input, output)
+{
+    database.getAll(input.params.table, function(json)
+    {
+        output.writeHead(200, {"Content-Type" : "text/html"});
+        output.end("<p>" + json + "</p>");
+    });
+});
+
+app.get("/:table/:key/:value", function(input, output)
+{
+    database.getID(input.params.table, input.params.key, input.params.value, function(json)
+    {
+        output.writeHead(200, {"Content-Type" : "text/html"});
+        output.end("<p>" + json + "</p>");
+    });
+});
 
 // On fait écouter le serveur sur le port 8080 :
-server.listen(8080);
+var server = app.listen(8080, function()
+{
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log("Example app listening at http://%s:%s", host, port);
+});
